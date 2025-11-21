656c69e687a51c44c5f258606b6d1e0346bec666
Tochi-Nwachukwu <tochinwachukwu33@gmail.com>
2025-11-21 09:31:35 +0100

    [init]- initialized the repository

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from monopoly-game-mcp!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/monopoly_board.py
@@ -0,0 +1,9 @@
+monopoly_board = [
+    {
+        id: 1,
+        "display_name": "Free Parking",
+        "payment_price": 0,
+        "rent_value": 0,
+        "purchase_value": 0,
+    }
+]
